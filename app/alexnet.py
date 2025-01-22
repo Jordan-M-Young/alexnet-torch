7b5b65cdcf7983bd6e7af8b97abfa7fc6dea9f63
Jordan Young <jordanyoung@Jordans-MacBook-Pro.local>
2025-01-22 15:33:21 -0600

    bias and weight initialization

--- app/alexnet.py
+++ app/alexnet.py
@@ -40,32 +40,72 @@ class AlexNet(torch.nn.Module):
         self.fc3 = torch.nn.Linear(4096, n_classes)
         self.re8 = torch.nn.ReLU()
 
-        self.soft = torch.nn.Softmax(dim=n_classes)
+        # first group, of conv layers, Section 3. Page 4-5
+        self.conv_net = torch.nn.Sequential(
+            self.c1,
+            self.re1,
+            self.lrn,
+            self.mx_pool1,
+            self.c2,
+            self.re2,
+            self.mx_pool2,
+            self.c3,
+            self.re3,
+            self.c4,
+            self.re4,
+            self.c5,
+            self.re5,
+            self.mx_pool3,
+        )
+
+        # fully connected layers group Section 3. Page 4-5
+        self.fully_connected = torch.nn.Sequential(
+            self.fc1,
+            self.re6,
+            self.dp1,
+            self.fc2,
+            self.re7,
+            self.dp2,
+            self.fc3,
+            self.re8,
+        )
+
+        # As discussed in Section 5. Page 6
+        self.init_biases()
+        self.init_weights()
 
     def forward(self, x):
         """Forward pass method."""
-        x = self.c1(x)
-        x = self.re1(x)
-        x = self.lrn(x)
-        x = self.mx_pool1(x)
-        x = self.c2(x)
-        x = self.re2(x)
-        x = self.mx_pool2(x)
-        x = self.c3(x)
-        x = self.re3(x)
-        x = self.c4(x)
-        x = self.re4(x)
-        x = self.c5(x)
-        x = self.re5(x)
-        x = self.mx_pool3(x)
+        x = self.conv_net(x)
         x = x.view(-1, 256 * 2 * 2)
-        x = self.fc1(x)
-        x = self.re6(x)
-        x = self.dp1(x)
-        x = self.fc2(x)
-        x = self.re7(x)
-        x = self.dp2(x)
-        x = self.fc3(x)
-        x = self.re8(x)
+        x = self.fully_connected(x)
 
         return x
+
+    def init_biases(self):
+        """Initialize biases according to paper methodology."""
+        # layers with biases initialized at 0.0 Section 5. Page 6
+        self.c1.bias.data.fill_(0.0)
+        self.c3.bias.data.fill_(0.0)
+
+        # layers with biasese initialized at 1.0 Section 5. Page 6
+        self.c2.bias.data.fill_(1.0)
+        self.c4.bias.data.fill_(1.0)
+        self.c5.bias.data.fill_(1.0)
+
+        # commented out, if you set these fc layers biases to 1's the model does not learn lol
+        # loss explodes till we return nans
+        # self.fc1.bias.data.fill_(1.0)
+        # self.fc2.bias.data.fill_(1.0)
+        # self.fc3.bias.data.fill_(1.0)
+
+    def init_weights(self):
+        """Recursively Initialize weight of layers."""
+        self.conv_net.apply(initialize_weights)
+        self.fully_connected.apply(initialize_weights)
+
+
+def initialize_weights(m: torch.nn.Module):
+    """Initialize weight of layer according to a normal distribution."""
+    if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
+        torch.nn.init.normal_(m.weight, mean=0, std=0.01)
